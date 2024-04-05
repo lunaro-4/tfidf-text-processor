2dc2622567bd60ec1c8ef2c3d1e626d598ffa6fb
lunaro-4 <pavelike@gmail.com>
2024-04-05 11:37:56 +0300

    feat(project): added library id input
    Added form and model fields for library id, added redirect to get with drguments in template

--- tfidf_text_processor/forms.py
+++ tfidf_text_processor/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(label='Select a file', help_text='max. 42 megabytes')
+    library_id = forms.CharField()
 
 
 
--- tfidf_text_processor/models.py
+++ tfidf_text_processor/models.py
@@ -7,5 +7,4 @@ from app.settings import MEDIA_URL
 class  InputTextFile(models.Model):
     file = models.FileField(upload_to='.')
     date = models.DateField(auto_now_add=True)
-    # library_id = models.ForeignKey()
-
+    library_id = models.CharField(max_length = 20, default= '0')
