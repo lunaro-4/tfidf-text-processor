b96d3ef3e5d4e7eb963b3beef67167c09e07d13b
lunaro-4 <pavelike@gmail.com>
2024-04-05 10:57:59 +0300

    feat(models): added model for file input

--- tfidf_text_processor/models.py
+++ tfidf_text_processor/models.py
@@ -7,4 +7,5 @@ from app.settings import MEDIA_URL
 class  InputTextFile(models.Model):
     file = models.FileField(upload_to='.')
     date = models.DateField(auto_now_add=True)
+    # library_id = models.ForeignKey()
 
